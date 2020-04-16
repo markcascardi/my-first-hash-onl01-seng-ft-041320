de307ad27f653232cc6b532f7b26ebe683b1464f
Mark Cascardi <mcascardi@gmail.com>
2020-04-16 21:38:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,8 +19,6 @@ def retrieval
                   "oil paintings" => 3
                  }
  shipping_manifest["oil paintings"]
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -28,15 +26,8 @@ def adding
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
+                 }  # add 2 muskets to the shipping_manifest hash below
  shipping_manifest["muskets"] = 2
  shipping_manifest["gun powder"] = 4
  shipping_manifest
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
 end
