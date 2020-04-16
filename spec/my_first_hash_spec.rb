eafb7867fad0f3e568c4a9abf34c32b47269c22c
Mark Cascardi <mcascardi@gmail.com>
2020-04-16 21:38:27 +0000

    Automatically backed up by Learn

--- spec/my_first_hash_spec.rb
+++ spec/my_first_hash_spec.rb
@@ -28,16 +28,16 @@ context "Challenge III: Retrieving Data" do
   end
 end
 
-# context "Challenge IV: Adding Data" do
-#   describe "#adding" do
-#     it "operates on the shipping_manifest hash to add a key/value pair" do
-#       expect(adding).to eq({
-#                       "whale bone corsets" => 5,
-#                       "porcelain vases" => 2,
-#                       "oil paintings" => 3,
-#                       "muskets" => 2,
-#                       "gun powder" => 4
-#                       })
-#     end
-#   end
-# end
+context "Challenge IV: Adding Data" do
+  describe "#adding" do
+    it "operates on the shipping_manifest hash to add a key/value pair" do
+      expect(adding).to eq({
+                      "whale bone corsets" => 5,
+                      "porcelain vases" => 2,
+                      "oil paintings" => 3,
+                      "muskets" => 2,
+                      "gun powder" => 4
+                      })
+    end
+  end
+end
