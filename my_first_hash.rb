f1910710127def7b1cdcb4cbb9b707125d48822a
Mark Cascardi <mcascardi@gmail.com>
2020-04-16 21:27:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,15 +4,20 @@ end
 
 
 def shipping_manifest
-  the_manifest = { "whale bone corsets" => 5,
-                   "porcelain vases" => 2,
-                   "oil paintings" => 4 }
+  the_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
+
 end
 
 def retrieval
-  shipping_manifest = { "whale bone corsets" => 5,
-                        "porcelain vases" => 2,
-                        "oil paintings" => 3 }
+  shipping_manifest = {
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
+                  "oil paintings" => 3
+                 }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
